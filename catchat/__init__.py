39d32d5ac6bc7128d3a8e8d58bef41542e86914c
Xingyu Guo <1344166268@qq.com>
2019-08-03 23:21:05 +0800

    更新

--- catchat/__init__.py
+++ catchat/__init__.py
@@ -1,8 +1,10 @@
+import logging
 import os
 import random
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 import click
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from .blueprints import *
 from .extensions import db, login_manager, csrf, moment, socketio, oauth
@@ -28,7 +30,36 @@ def create_app(config_name=None):
 
 
 def register_logging(app=None):
-    pass
+    app.logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - "%(pathname)s", line:%(lineno)s - %(message)s')
+    file_handler = RotatingFileHandler('logs/data.log', maxBytes=10 * 1024 * 1024, backupCount=10)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+
+    class RequestFormatter(logging.Formatter):
+        def format(self, record):
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+            return super(RequestFormatter, self).format(record)
+
+    request_formatter = RequestFormatter(
+        '[%(asctime)s] - "%(remote_addr)s : %(url)s" - %(levelname)s - "%(pathname)s", line:%(lineno)s - %(message)s'
+    )
+
+    mail_handler = SMTPHandler(
+        mailhost=app.config['MAIL_SERVER'],
+        fromaddr=app.config['MAIL_USERNAME'],
+        toaddrs=app.config['ALBUMY_ADMIN_EMAIL'],
+        subject='CatChat程序错误',
+        credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    )
+    mail_handler.setFormatter(request_formatter)
+    mail_handler.setLevel(logging.ERROR)
+
+    if not app.debug:
+        app.logger.addHandler(file_handler)
+        app.logger.addHandler(mail_handler)
 
 
 def register_extensions(app=None):
